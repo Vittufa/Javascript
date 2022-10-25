26115b93a48aa2360e399a3d4602c3a0dcaa4e64
Vittufa <110200153+Vittufa@users.noreply.github.com>
2022-10-25 19:17:49 -0300

    Add files via upload

---
+++ open.js
@@ -0,0 +1,12 @@
+const lista = [
+    nombre ="Vittorio",
+    edad = 25,
+    desarrollador = true,
+    nacimiento = new Date(1997, 1, 26),
+    libro = {
+        nombre: "Odisea",
+        autor: "Homero",
+        fecha: 1837,
+        url: "www.culturagenial.com/es/la-odisea-de-homero/"
+        }
+]
